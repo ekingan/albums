c37c97fbdb81d59ed44864ea645e7eac2d759f9f
Emily Kingan <ekingan@Emilys-MacBook-Air.local>
2016-11-03 13:59:13 -0700

    url

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress }) => {
   const { buttonStyle, textStyle } = styles;
   return (
-    <TouchableOpacity style={buttonStyle} onPress={() => console.log('pressed')}>
+    <TouchableOpacity style={buttonStyle} onPress={onPress}>
       <Text style={textStyle}>Click For More</Text>
     </TouchableOpacity>
   );
